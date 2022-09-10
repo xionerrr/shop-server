bc0986861f0907a1d03c1ef1041817a0ad21cdf0
unknown <vkrivohub@gmail.com>
2022-09-11 00:57:36 +0300

    init orm and auth routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 
-import { TestModule } from './modules/Test/test.module'
+import { TestModule } from './modules/test/test.module'
+import { AuthModule } from './modules/auth/auth.module'
+import { PrismaModule } from './modules/prisma/prisma.module'
 
 @Module({
-  imports: [TestModule],
+  imports: [TestModule, AuthModule, PrismaModule],
 })
 export class AppModule {}
